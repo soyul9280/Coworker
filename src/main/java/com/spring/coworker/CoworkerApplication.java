386ef533919099334e77783d5939ee692efb53d4
soyul <soyul9280@gmail.com>
2025-09-28 11:07:38 +0900

    Chore:gitignore 작업 - 환경변수, sql, macOS시스템파일 추가

---
+++ src/main/java/com/spring/coworker/CoworkerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.coworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CoworkerApplication.class, args);
+  }
+
+}
