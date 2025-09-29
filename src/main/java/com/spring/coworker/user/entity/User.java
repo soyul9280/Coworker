0c97f88d3d661ee870bf4421fb7ecfc1ac7a89bf
soyul <soyul9280@gmail.com>
2025-09-29 23:49:45 +0900

    Feat: 유저 DTO생성

--- src/main/java/com/spring/coworker/user/entity/User.java
+++ src/main/java/com/spring/coworker/user/entity/User.java
@@ -3,6 +3,7 @@ package com.spring.coworker.user.entity;
 import com.vladmihalcea.hibernate.type.json.JsonType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -12,16 +13,21 @@ import jakarta.persistence.Table;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.Type;
 import org.hibernate.type.SqlTypes;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "users")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)//builder로 만들도록 직접생성 막음
+@EntityListeners(AuditingEntityListener.class)
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
