a9b119953f51d20b41ffb3d0100070528810d0cd
soyul <soyul9280@gmail.com>
2025-10-03 20:26:59 +0900

    Feat:article 조회 커스텀repository

--- src/main/java/com/spring/coworker/article/repository/ArticleRepository.java
+++ src/main/java/com/spring/coworker/article/repository/ArticleRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, UUID> {
+public interface ArticleRepository extends JpaRepository<Article, UUID>,ArticleCustomRepository {
   boolean existsByTitle(String title);
 }
